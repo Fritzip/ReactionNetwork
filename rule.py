299f081ef298040a2755cccce605f7ca09aa5903
Friedrich Hemer <friedrich.hemer@gmail.com>
2015-01-09 10:55:18 +0100

    define rules

---
+++ rule.py
@@ -0,0 +1,35 @@
+import re
+
+class Rule():
+	"""Transform a formated rule into stochastic matrix"""
+	def __init__(self, rule):
+		self.rule = rule
+		left, right = self.rule.replace(' ','').split('=')
+
+		if len(left) == 4:
+			self.l1, self.l2 = left[0:2], left[2:4]
+			self.op1 = '.'
+		elif len(left) == 5:
+			self.op1 = left[2]
+			self.l1, self.l2 =  left.split(self.op1)
+		else:
+			print "Error : not valid rule"
+
+		if len(right) == 4:
+			self.r1, self.r2 = right[0:2], right[2:4]
+			self.op2 = '.'
+		elif len(right) == 5:
+			self.op2 = right[2]
+			self.r1, self.r2 =  right.split(self.op2)
+		else:
+			print "Error : not valid rule"
+
+
+		print self.l1, self.op1, self.l2, ' = ', self.r1, self.op2, self.r2
+
+	def __repr__(self):
+		return self.rule
+
+if __name__ == '__main__':
+	r = Rule("a0+a1 = a1a2")
+	print r		
