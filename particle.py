3eab43ccc8d8a568c80800aefff2136536c508e9
Friedrich Hemer <friedrich.hemer@gmail.com>
2015-01-13 11:36:07 +0100

    alternative constructor

--- particle.py
+++ particle.py
@@ -6,6 +6,11 @@ class Particle():
 		self.type = type
 		self.state = state
 
+	@classmethod
+	def from_stype(cls, id, stype):
+		"""Initialize particle from stype"""
+		return cls(id, stype[0], int(stype[1]))
+
 	def stype(self):
 		""" Letter/number formalism """
 		return self.type+str(self.state)
@@ -16,3 +21,6 @@ class Particle():
 if __name__ == '__main__':
 	p = Particle(4, 'a', 0)
 	print p
+
+	q = Particle.from_stype(1, 'a0')
+	print q
